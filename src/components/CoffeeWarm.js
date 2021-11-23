71d85f271996a91e2f49d69d5b04bd3495e9d80c
julienshim <25723428+julienshim@users.noreply.github.com>
2021-11-22 22:32:47 -0800

    defratio

--- src/components/CoffeeWarm.js
+++ src/components/CoffeeWarm.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+
+const defaultRatio = {
+  coffee: 11,
+  water: 10,
+  brew: 150,
+};
 
 const CoffeeWarm = () => {
-    return (
-        <h1>Iced Cofee</h1>
-    )
-}
+  return <h1>Iced Cofee</h1>;
+};
 
 export default CoffeeWarm;
