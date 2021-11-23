8108557eeb115cf8ccfffd912fa90237a35fb25e
julienshim <25723428+julienshim@users.noreply.github.com>
2021-11-22 22:33:35 -0800

    default ratio

--- src/components/LatteWarm.js
+++ src/components/LatteWarm.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+const defaultRatio = {
+    coffee: 18,
+    simpleSyrup: 10,
+    water: 90,
+    milk: 176,
+    brew: 90
+};
+
 const LatteWarm = () => {
     return (
         <h1>Iced Cofee</h1>
