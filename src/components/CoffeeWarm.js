2bd1787978f51017f46dce94561a81584f5c3f8d
julienshim <25723428+julienshim@users.noreply.github.com>
2021-11-26 23:49:57 -0800

    definit

--- src/components/CoffeeWarm.js
+++ src/components/CoffeeWarm.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const defaultRatio = {
   coffee: 11,
-  water: 10,
-  brew: 150,
+  waterAbsorptionLoss: 11 * 2,
+  water: 200,
+  output: 200 - (11 * 2)
 };
+
+console.log(defaultRatio.output)
 
 const CoffeeWarm = () => {
   return <h1>Iced Cofee</h1>;
