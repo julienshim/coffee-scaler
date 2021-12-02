276ca7a63fb21a37a01d2845491f64beea590def
julienshim <25723428+julienshim@users.noreply.github.com>
2021-12-02 13:52:07 -0800

    coffee warm init form

--- src/components/CoffeeWarm.js
+++ src/components/CoffeeWarm.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 
 const defaultRatio = {
   coffee: 11,
   waterAbsorptionLoss: 11 * 2,
   water: 200,
-  output: 200 - (11 * 2)
+  output: 200 - 11 * 2,
 };
 
-console.log(defaultRatio.output)
+console.log(defaultRatio.output);
 
 const CoffeeWarm = () => {
-  return <h1>Iced Cofee</h1>;
+  const [coffee, setCoffee] = useState(defaultRatio.coffee);
+  const [waterAbsorptionLoss, setWaterAbsorptionLoss] = useState(
+    defaultRatio.waterAbsorptionLoss
+  );
+  const [water, setWater] = useState(defaultRatio.water);
+  const [output, setOutput] = useState(defaultRatio.output);
+
+  return (
+    <Fragment>
+      <h1>Coffee Warm</h1>
+      <form>
+        <input type="text" value={coffee} />
+        <input type="text" value={water} />
+        <input type="text" value={output} />
+      </form>
+    </Fragment>
+  );
 };
 
 export default CoffeeWarm;
