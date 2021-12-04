82439eb4a3ae65f4cccb2569639aabad1c953884
julienshim <25723428+julienshim@users.noreply.github.com>
2021-12-03 21:43:35 -0800

    coffee recalc

--- src/components/CoffeeWarm.js
+++ src/components/CoffeeWarm.js
@@ -17,13 +17,41 @@ const CoffeeWarm = () => {
   const [water, setWater] = useState(defaultRatio.water);
   const [output, setOutput] = useState(defaultRatio.output);
 
+  const recalculate = (event) => {
+    const name = event.target['name'];
+    const value = event.target.value;
+
+    if (name === "output") {
+      setOutput(value);
+    }
+    if (name === "coffee") {
+      setCoffee(value);
+      setWaterAbsorptionLoss(value * 2);
+      setWater((value * defaultRatio.water) / defaultRatio.coffee);
+      setOutput((value * defaultRatio.water) / defaultRatio.coffee - value * 2);
+    }
+    if (name === "water") {
+      setWater(value);
+    }
+  };
+
   return (
     <Fragment>
       <h1>Coffee Warm</h1>
       <form>
-        <input type="text" value={coffee} />
-        <input type="text" value={water} />
-        <input type="text" value={output} />
+        <input
+          type="text"
+          value={coffee}
+          name="coffee"
+          onChange={recalculate}
+        />
+        <input type="text" value={water} name="water" Change={recalculate} />
+        <input
+          type="text"
+          value={output}
+          name="output"
+          onChange={recalculate}
+        />
       </form>
     </Fragment>
   );
