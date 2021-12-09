4bb6b3c1c6eca26b43c262e8b0d1dc48dd02c6d6
julienshim <25723428+julienshim@users.noreply.github.com>
2021-12-08 21:54:13 -0800

    components init

--- src/components/CoffeeIced.js
+++ src/components/CoffeeIced.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 const defaultRatio = {
-    coffee: 11,
-    ice: 80,
-    output: (120 + 80) - 11 * 2,
-    water: 120,
-    waterLossed: 11 * 2,
+  coffee: 11,
+  ice: 80,
+  output: 120 + 80 - 11 * 2,
+  water: 120,
+  waterLossed: 11 * 2,
 };
 
 const CoffeeIced = () => {
-    return (
-        <h1>Iced Cofee</h1>
-    )
-}
+  return (
+    <Fragment>
+      <h1>Coffee Iced</h1>
+      <form>
+        <label>coffee</label>
+        <input type="text" name="coffee" />
+        <label>water</label>
+        <input type="text" name="water" />
+        <label>ice</label>
+        <input type="text" name="ice" />
+        <label>output</label>
+        <input type="text" name="output" />
+      </form>
+    </Fragment>
+  );
+};
 
 export default CoffeeIced;
