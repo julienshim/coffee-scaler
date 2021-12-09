d6050a513c8c6ee82201605c4241a34a94448ec6
julienshim <25723428+julienshim@users.noreply.github.com>
2021-12-08 21:55:07 -0800

    latte warm init input

--- src/components/LatteWarm.js
+++ src/components/LatteWarm.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 const defaultRatio = {
-    coffee: 18,
-    waterLossed: 18 * 2,
-    simpleSyrup: 10,
-    water: 90,
-    milk: 176,
-    output: (90 + 176 + 10) - (18 * 2)
+  coffee: 18,
+  waterLossed: 18 * 2,
+  simpleSyrup: 10,
+  water: 90,
+  milk: 176,
+  output: 90 + 176 + 10 - 18 * 2,
 };
 
 const LatteWarm = () => {
-    return (
-        <h1>Iced Cofee</h1>
-    )
-}
+  return (
+    <Fragment>
+      <h1>Latte Warm</h1>
+      <form>
+        <label>coffee</label>
+        <input type="text" name="coffee" />
+        <label>water</label>
+        <input type="text" name="water" />
+        <label>simple syrup</label>
+        <input type="text" name="simpleSyrup" />
+        <label>output</label>
+        <input type="text" name="output" />
+      </form>
+    </Fragment>
+  );
+};
 
 export default LatteWarm;
