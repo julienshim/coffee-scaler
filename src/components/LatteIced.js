c5aa7a7c2ce754515b638eae9682119c09036ccc
julienshim <25723428+julienshim@users.noreply.github.com>
2021-12-08 21:54:45 -0800

    latte iced init component

--- src/components/LatteIced.js
+++ src/components/LatteIced.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 const defaultRatio = {
-    coffee: 18,
-    waterLossed: 18 * 2,
-    simpleSyrup: 10,
-    water: 90,
-    milk: 176,
-    ice: 56,
-    output: (90 + 10 + 176) - (18 * 2)
+  coffee: 18,
+  waterLossed: 18 * 2,
+  simpleSyrup: 10,
+  water: 90,
+  milk: 176,
+  ice: 56,
+  output: 90 + 10 + 176 - 18 * 2,
 };
 
 const LatteIced = () => {
-    return (
-        <h1>Iced Cofee</h1>
-    )
-}
+  return (
+    <Fragment>
+      <h1>Latte Iced</h1>
+      <form>
+        <label>coffee</label>
+        <input type="text" name="coffee" />
+        <label>water</label>
+        <input type="text" name="water" />
+        <label>milk</label>
+        <input type="text" name="milk" />
+        <label>simple syrup</label>
+        <input type="text" name="simpleSyrup" />
+        <label>output</label>
+        <input type="text" name="output" />
+      </form>
+    </Fragment>
+  );
+};
 
 export default LatteIced;
